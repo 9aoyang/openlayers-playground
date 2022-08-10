63ded4d6725220917c56e54206a10f042cea60b8
silence1amb <9aoyang@gmail.com>
2022-08-10 10:10:12 +0800

    feat: initial

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+<ol-map :loadTilesWhileAnimating="true" :loadTilesWhileInteracting="true" style="height:400px">
+
+    <ol-view ref="view" :center="center" :rotation="rotation" :zoom="zoom"
+    :projection="projection" />
+
+    <ol-tile-layer>
+        <ol-source-osm />
+    </ol-tile-layer>
+
+</ol-map>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+
+export default {
+    setup() {
+        const center = ref([40, 40])
+        const projection = ref('EPSG:4326')
+        const zoom = ref(8)
+        const rotation = ref(0)
+        return {
+            center,
+            projection,
+            zoom,
+            rotation
+        }
+    },
+}
+</script>
+
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
